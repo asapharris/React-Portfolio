a0db58a5314dc86b4f8eaafd7f5db5793fe36318
Jamie <asapharris88@gmail.com>
2023-01-28 21:56:50 -0500

    inital setup starter code/ set up routes in navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            Jamie Harris
+            <i class='fab fa-typo3' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/portfolio'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/resume'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Resume
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/contact-me'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Contact Me
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
+
+
